a9ae04933479e37401a930b6281f8b869ce4bea1
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-10 19:34:38 +0200

    in progress dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,36 @@
 const userMail = localStorage.getItem('user_mail');
 const userId = localStorage.getItem('user_id');
+const userName = localStorage.getItem('user_name');
 
 if (!userMail || !userId) {
   window.location.href = 'https://mediamaster.ieti.site/';
 }
 
 $(function() {
-    
+  $("#welcome_messagge").text("Welcome, " + userName);
+
+  function getUsersList(user_mail, user_id) {
+    $.ajax({
+      url: 'https://mediamaster.ieti.site/viewUserLists',
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        user_mail: user_mail,
+        user_id: user_id
+      },
+    })
+      .done(function (data) {
+        listas = data;
+        $("#myLists").empty();
+        data.forEach(function (list) {
+          $("#myList").append("<li><h3>" + list.list_name + "</h3>" + "<a href='https://mediamaster.ieti.site/viewDetailed?id=" + list.list_id + "'><ul></ul></a></li>");
+          $("#myList").append("<li>Movies: " + list.movie_id + "</li>");
+          $("#myList").append("<li>Series: " + list.serie_id + "</li>");
+          $("#myList").append("<li>Books: " + list.book_id + "</li>");
+          $("#myList").append("<li>Games: " + list.game_id + "</li>");
+        });
+      });
+  }
 });
 
 
