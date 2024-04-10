773c342606e43d5b3dbc0cbfda274e3a85e1b263
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-10 20:25:26 +0200

    deleted debug in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,10 +6,6 @@ if (!userMail || !userId) {
   window.location.href = 'https://mediamaster.ieti.site/';
 }
 
-console.log(userMail);
-console.log(userId);
-console.log(userName);
-
 $(function() {
   $("#welcome_messagge").text("Welcome, " + userName);
 
