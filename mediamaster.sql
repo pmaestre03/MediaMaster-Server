f2dd6f676781c22646a63d9f9cb3c02b465ef432
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2024-04-30 18:02:46 +0200

    changed database script

--- mediamaster.sql
+++ mediamaster.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `mediamaster`
 --
+DROP DATABASE IF EXISTS `mediamaster`
+
 CREATE DATABASE IF NOT EXISTS `mediamaster` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `mediamaster`;
 
